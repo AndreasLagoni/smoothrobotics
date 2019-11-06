1dd589776c431528062d52eb968044d33fdb108d
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-11-06 13:52:14 +0100

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,179 @@
+<?php get_header();?>
+<main class="contentWrapper">
+
+<section class="headSection-wrapper">
+    <article class="headSection-info">
+        <div class="headSection-innerInfo">
+        <h2 class="header-bg-h1">Tidsløse<br>Designs <span class="header-dot">.</span></h2>
+    <h2 class="header-h2">Halløj, Det er mig</h2>
+    <h1 class="headername">Jeppe Adler <span class="header-dot blue">.</span></h1>
+    <p>Entustiastisk multimediedesigner 
+studerende, med passion for aktuelle, 
+tidsløse og kreative designs, der ser frem 
+til at forme fremtidens front-end udvikling, 
+ved at skabe fleksible User-Interfaces, 
+som sammenfletter interaktion med 
+information og data.</p>
+<a href="#skillSection" class="std-link underscoreLink">SCROLL FOR MERE</a></div>
+    
+    <img src="./wp-content/themes/newtheme/assets/jeppeimg1x.png" alt="">
+</article>
+</section>
+<section class="skillsSection-wrapper" id="skillSection">
+<article class="skills-article1">
+    <h3 class="skills-h3">Mine Skills</h3>
+    <h2>Grafisk Design, 
+Interface Design, User 
+Experience & kodning.</h2>
+</article>
+<article class="skills-article2">
+    <p>Jeg specialisere mig i at designe og udarbejde komplekse web-aplikationer, digitale produkt designs, koncepter og produktfotografering. 
+Jeg gør en dyd ud af at udvikle enkle og funktionelle brugerflader, i tidsløse og kreative designs. 
+Jeg fascineres af hele processen hvor man går fra et koncept, til udvikling og lancering og indsamle brugers feedback på klientens produkter såvel som på mine egne.
+I mit uddannelses forløb har vi arbejdet med flere virksomheder løbende. Her forsøger jeg altid at gå så meget i dialog med klienten som muligt, for og skabe et bedst muligt samarbejde. Jeg tror på at man skaber det bedst mulige produkt, som et hold. </p>
+</article>
+</section>
+<section class="processSection-wrapper">
+    <article class="process-article1">
+        <h2>Processen</h2>
+        <img src="./wp-content/themes/newtheme/assets/process.png" alt="">
+    </article>
+
+<article class="processSection-contentWrapper">
+    <div class="processSection-processItem">
+        <h2 class="process-bg-text">01</h2>
+        <div class="processSection-processInnerContent">
+            <h3>Discovery</h3>
+            <div class="processParagraph">
+            <p>Indsaml Information</p>
+            <p>Analysere</p>
+            <p>Personaer</p>
+            <p>Målsætning</p>
+        </div>
+        </div>
+    </div>
+    <div class="processSection-processItem">
+        <h2 class="process-bg-text">02</h2>
+        <div class="processSection-processInnerContent">
+            <h3>Strategi & Planlægning</h3>
+            <div class="processParagraph">
+            <p>Projektets Tidsplan</p>
+            <p>Content Planlægning</p>
+            <p>Journey maps/CJM</p>
+            <p>Struktur</p>
+        </div>
+        </div>
+    </div>
+    <div class="processSection-processItem">
+        <h2 class="process-bg-text">03</h2>
+        <div class="processSection-processInnerContent">
+            <h3>Design
+</h3>
+            <div class="processParagraph">
+            <p>Wireframe</p>
+            <p>Moodboard</p>
+            <p>Prototype</p>
+            <p>Interne Tests</p>
+        </div>
+        </div>
+    </div>
+    <div class="processSection-processItem">
+        <h2 class="process-bg-text">04</h2>
+        <div class="processSection-processInnerContent">
+            <h3>Web & Udvikling
+</h3>
+            <div class="processParagraph">
+            <p>Kodning</p>
+            <p>Responsivitet</p>
+            <p>SEO</p>
+            <p>Content</p>
+        </div>
+        </div>
+    </div>
+    <div class="processSection-processItem">
+        <h2 class="process-bg-text">05</h2>
+        <div class="processSection-processInnerContent">
+            <h3>Tests
+</h3>
+            <div class="processParagraph">
+            <p>Cross Platform</p>
+            <p>Endelig klient Feedback</p>
+            <p>Hastighed & Validering</p>
+            <p>Browser Test</p>
+        </div>
+        </div>
+    </div>
+    <div class="processSection-processItem">
+        <h2 class="process-bg-text">06</h2>
+        <div class="processSection-processInnerContent">
+            <h3>Lancering & Vedligeholdelse
+</h3>
+            <div class="processParagraph">
+            <p>Forbedring</p>
+            <p>Endelig Accept</p>
+            <p>Launch</p>
+            <p>Vedligeholdelse</p>
+        </div>
+        </div>
+    </div>
+    
+</article>
+</section>
+<section class="projectsSection-wrapper">
+<h2>Tidligere<br>projekter <span><img src="./wp-content/themes/newtheme/assets/desktop.png" alt=""></span></h2>
+<article class="projectsSection-casesWrapper">
+    <div class="projectsSection-caseItem item01">
+    <a href="#">
+        <img src="./wp-content/themes/newtheme/assets/case01.png" alt="">
+    </a>
+    </div>
+    <div class="projectsSection-caseItem item02">
+    <a href="#">
+        <img src="./wp-content/themes/newtheme/assets/case02.png" alt="">
+    </a>
+    </div>
+    <div class="projectsSection-caseItem item03">
+    <a href="#">
+        <img src="./wp-content/themes/newtheme/assets/case03.png" alt="">
+    </a>
+    </div>
+</article>
+</section>
+<section class="careerSection-wrapper">
+<article class="careerSection-careerItem">
+    <h3>Netto</h3>
+    <p>Butiksmedarbejder</p>
+    <p>2014-2015</p>
+</article>
+<article class="careerSection-careerItem">
+    <h3>Café Razz</h3>
+    <p>Ansvarligt Barpersonale</p>
+    <p>Ledende Bartender</p>
+    <p>2014-2015</p>
+</article>
+<article class="careerSection-careerItem">
+    <h3>Elgiganten</h3>
+    <p>Sælger</p>
+    <p>2014-2015</p>
+</article>
+</section>
+<section class="educationSection-wrapper">
+<article class="educationSection-educationItem">
+    <h3>Folkeskole</h3>
+    <p>10 år</p>
+    <p>2014-2015</p>
+</article>
+<article class="educationSection-educationItem">
+    <h3>Gymnasium</h3>
+    <p>3 år</p>
+    <p>2014-2015</p>
+</article>
+<article class="educationSection-educationItem">
+    <h3>Multimediedesigner</h3>
+    <p>2 år</p>
+    <p>2014-2015</p>
+</article>
+<a href="#">DOWNLOAD MIT CV</a>
+</section>
+</main>
+<?php get_footer();?>
---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function load_stylesheets() {
+
+
+wp_register_style("stylesheet", get_template_directory_uri() . "./css/main.css", array(), rand(111,9999), 'all');
+wp_enqueue_style("stylesheet");
+}
+
+add_action("wp_enqueue_scripts", "load_stylesheets");
+?>
