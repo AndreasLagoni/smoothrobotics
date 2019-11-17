82573ba987091d4a35e95cc02db2deaffe6b39a5
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-11-17 13:11:50 +0100

    frontpage html

--- front-page.php
+++ front-page.php
@@ -1,2 +1,167 @@
 <?php get_header();?>
+<main class="wrapper">
+<section class="frontbanner-wrapper" style="background-image: url('http://localhost/wordpress/wp-content/uploads/2019/11/Group-248-1.png')">
+<div class="innerfrontbanner-wrapper">
+    <div class="innerbanner-header">
+    <h1>Smooth Robotics</h1>
+    <h2>Fremtidens Automatisering</h2>
+    </div>
+    <div class="innerbanner-text">
+    <p>Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit, 
+sed do eiusmod tempor 
+incididunt ut labore et dolore 
+magna aliqua. Tristique 
+sollicitudin nibh sit amet 
+commodo nulla. Sed blandit 
+libero volutpat sed cras 
+ornare arcu dui.</p>
+<p>Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit, 
+sed do eiusmod tempor 
+incididunt ut labore et dolore 
+magna aliqua. Tristique 
+sollicitudin nibh sit amet 
+commodo nulla. Sed blandit 
+libero volutpat sed cras 
+ornare arcu dui.</p>
+    </div>
+</div>
+</section>
+<section class="frontpage-underheader-wrapper">
+<article class="frontpage-underheader-item">
+<div class="underheader-item-banner"></div>
+<div class="underheader-item-content">
+<h3>New spade to smoothtool</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+</div>
+<div class="underheader-item-link"></div>
+</article>
+<article class="frontpage-underheader-item">
+<div class="underheader-item-banner"></div>
+<div class="underheader-item-content">
+<h3>New spade to smoothtool</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+</div>
+<div class="underheader-item-link"></div>
+</article>
+<article class="frontpage-underheader-item">
+<div class="underheader-item-banner"></div>
+<div class="underheader-item-content">
+<h3>New spade to smoothtool</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+</div>
+<div class="underheader-item-link"></div>
+</article>
+</section>
+<section class="smoothtool-quickinfo-wrapper">
+<article class="smoothtool-quickinfo-item">
+<h2>Smooth Tool. </h2>
+<h3>- Made by welders, for welders.</h3>
+</article>
+<article class="smoothtool-quickinfo-item">
+<p>Lorem ipsum dolor sit amet, consectetur 
+adipiscing elit, sed do eiusmod tempor 
+incididunt ut labore et dolore magna 
+aliqua. Tristique sollicitudin nibh sit amet 
+commodo nulla. Sed blandit libero 
+volutpat sed cras ornare arcu dui. 
+Fermentum odio eu feugiat pretium nibh 
+ipsum consequat nisl. Enim sed faucibus 
+turpis in eu mi bibendum neque. Sit amet 
+luctus venenatis lectus magna fringilla. 
+Neque gravida in fermentum et sollicitudin 
+ac. </p></article>
+<article class="smoothtool-quickinfo-item"><p>Lorem ipsum dolor sit amet, consectetur 
+adipiscing elit, sed do eiusmod tempor 
+incididunt ut labore et dolore magna 
+aliqua. Tristique sollicitudin nibh sit amet 
+commodo nulla. Sed blandit libero 
+volutpat sed cras ornare arcu dui. 
+Fermentum odio eu feugiat pretium nibh 
+ipsum consequat nisl. Enim sed faucibus 
+turpis in eu mi bibendum neque. Sit amet 
+luctus venenatis lectus magna fringilla. 
+Neque gravida in fermentum et sollicitudin 
+ac. </p></article>
+</section>
+<section class="smoothtool-maincontent">
+<article class="smoothtool-maincontent-banner">
+</article>
+<article class="smoothtool-process-wrapper">
+<div class="smoothtool-process-item">
+<h3>Plan</h3>
+<p>Plan the whole process 
+from welding to finish 
+product</p>
+<div class="arrowright-wrapper"></div>
+</div>
+<div class="smoothtool-process-item"><h3>Plan</h3>
+<p>Plan the whole process 
+from welding to finish 
+product</p>
+<div class="arrowright-wrapper"></div></div>
+<div class="smoothtool-process-item"><h3>Plan</h3>
+<p>Plan the whole process 
+from welding to finish 
+product</p>
+<div class="arrowright-wrapper"></div></div>
+<div class="smoothtool-process-item"><h3>Plan</h3>
+<p>Plan the whole process 
+from welding to finish 
+product</p>
+<div class="arrowright-wrapper"></div></div>
+<div class="smoothtool-process-item"><h3>Plan</h3>
+<p>Plan the whole process 
+from welding to finish 
+product</p>
+<div class="arrowright-wrapper"></div></div>
+</article>
+</section>
+<section class="frontpage-info-aboutus">
+<article class="info-aboutus-row1">
+<h2>What we do
+for you</h2>
+</article>
+<article class="info-aboutus-row2">
+<div class="info-aboutus-item">
+<h3>Save time and effort.</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Sed cras ornare arcu dui vivamus arcu. sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. </p>
+</div>
+<div class="info-aboutus-item">
+<h3>Save time and effort.</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Sed cras ornare arcu dui vivamus arcu. sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. </p>
+</div>
+<div class="info-aboutus-item">
+<h3>Save time and effort.</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Sed cras ornare arcu dui vivamus arcu. sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. </p>
+</div>
+<div class="info-aboutus-item">
+<h3>Save time and effort.</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Sed cras ornare arcu dui vivamus arcu. sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. </p>
+</div>
+</article>
+</section>
+<section class="section-numbers">
+<article class="numbers-article-item"></article>
+<article class="numbers-article-item"></article>
+<article class="numbers-article-item"></article>
+</section>
+<section class="section-partners-wrapper">
+
+</section>
+</main>
+
 <?php get_footer();?>
