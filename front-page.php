4e64a68074ae1ad7d1f4dcdbce416d88b57d2346
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-11-17 15:10:25 +0100

    mere frontpage

--- front-page.php
+++ front-page.php
@@ -7,10 +7,7 @@
     <h2>Fremtidens Automatisering</h2>
     </div>
     <div class="innerbanner-text">
-    <p>Lorem ipsum dolor sit amet, 
-consectetur adipiscing elit, 
-sed do eiusmod tempor 
-incididunt ut labore et dolore 
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
 magna aliqua. Tristique 
 sollicitudin nibh sit amet 
 commodo nulla. Sed blandit 
@@ -29,94 +26,98 @@ ornare arcu dui.</p>
 </div>
 </section>
 <section class="frontpage-underheader-wrapper">
-<article class="frontpage-underheader-item">
-<div class="underheader-item-banner"></div>
-<div class="underheader-item-content">
-<h3>New spade to smoothtool</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-</div>
-<div class="underheader-item-link"></div>
-</article>
-<article class="frontpage-underheader-item">
-<div class="underheader-item-banner"></div>
-<div class="underheader-item-content">
-<h3>New spade to smoothtool</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-</div>
-<div class="underheader-item-link"></div>
-</article>
-<article class="frontpage-underheader-item">
-<div class="underheader-item-banner"></div>
-<div class="underheader-item-content">
-<h3>New spade to smoothtool</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-</div>
-<div class="underheader-item-link"></div>
-</article>
+    <article class="frontpage-underheader-item">
+        <div class="underheader-item-banner"></div>
+        <div class="underheader-item-content">
+        <h3>New spade to smoothtool</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        </div>
+        <div class="underheader-item-link"></div>
+    </article>
+    <article class="frontpage-underheader-item">
+    <div class="underheader-item-banner"></div>
+    <div class="underheader-item-content">
+    <h3>New spade to smoothtool</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    </div>
+    <div class="underheader-item-link"></div>
+    </article>
+    <article class="frontpage-underheader-item">
+    <div class="underheader-item-banner"></div>
+    <div class="underheader-item-content">
+    <h3>New spade to smoothtool</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    </div>
+    <div class="underheader-item-link"></div>
+    </article>
+</section>
+<section class="section-breaker">
+    <article class="thebreaker"></article>
 </section>
 <section class="smoothtool-quickinfo-wrapper">
-<article class="smoothtool-quickinfo-item">
-<h2>Smooth Tool. </h2>
-<h3>- Made by welders, for welders.</h3>
-</article>
-<article class="smoothtool-quickinfo-item">
-<p>Lorem ipsum dolor sit amet, consectetur 
-adipiscing elit, sed do eiusmod tempor 
-incididunt ut labore et dolore magna 
-aliqua. Tristique sollicitudin nibh sit amet 
-commodo nulla. Sed blandit libero 
-volutpat sed cras ornare arcu dui. 
-Fermentum odio eu feugiat pretium nibh 
-ipsum consequat nisl. Enim sed faucibus 
-turpis in eu mi bibendum neque. Sit amet 
-luctus venenatis lectus magna fringilla. 
-Neque gravida in fermentum et sollicitudin 
-ac. </p></article>
-<article class="smoothtool-quickinfo-item"><p>Lorem ipsum dolor sit amet, consectetur 
-adipiscing elit, sed do eiusmod tempor 
-incididunt ut labore et dolore magna 
-aliqua. Tristique sollicitudin nibh sit amet 
-commodo nulla. Sed blandit libero 
-volutpat sed cras ornare arcu dui. 
-Fermentum odio eu feugiat pretium nibh 
-ipsum consequat nisl. Enim sed faucibus 
-turpis in eu mi bibendum neque. Sit amet 
-luctus venenatis lectus magna fringilla. 
-Neque gravida in fermentum et sollicitudin 
-ac. </p></article>
+    <article class="smoothtool-quickinfo-item item-header">
+    <h2>Smooth Tool. </h2>
+    <h3>- Made by welders, for welders.</h3>
+    </article>
+    <article class="smoothtool-quickinfo-item">
+    <p>Lorem ipsum dolor sit amet, consectetur 
+    adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna 
+    aliqua. Tristique sollicitudin nibh sit amet 
+    commodo nulla. Sed blandit libero 
+    volutpat sed cras ornare arcu dui. 
+    Fermentum odio eu feugiat pretium nibh 
+    ipsum consequat nisl. Enim sed faucibus 
+    turpis in eu mi bibendum neque. Sit amet 
+    luctus venenatis lectus magna fringilla. 
+    Neque gravida in fermentum et sollicitudin 
+    ac. </p></article>
+    <article class="smoothtool-quickinfo-item"><p>Lorem ipsum dolor sit amet, consectetur 
+    adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna 
+    aliqua. Tristique sollicitudin nibh sit amet 
+    commodo nulla. Sed blandit libero 
+    volutpat sed cras ornare arcu dui. 
+    Fermentum odio eu feugiat pretium nibh 
+    ipsum consequat nisl. Enim sed faucibus 
+    turpis in eu mi bibendum neque. Sit amet 
+    luctus venenatis lectus magna fringilla. 
+    Neque gravida in fermentum et sollicitudin 
+    ac. </p></article>
 </section>
 <section class="smoothtool-maincontent">
-<article class="smoothtool-maincontent-banner">
-</article>
-<article class="smoothtool-process-wrapper">
-<div class="smoothtool-process-item">
-<h3>Plan</h3>
-<p>Plan the whole process 
-from welding to finish 
-product</p>
-<div class="arrowright-wrapper"></div>
-</div>
-<div class="smoothtool-process-item"><h3>Plan</h3>
-<p>Plan the whole process 
-from welding to finish 
-product</p>
-<div class="arrowright-wrapper"></div></div>
-<div class="smoothtool-process-item"><h3>Plan</h3>
-<p>Plan the whole process 
-from welding to finish 
-product</p>
-<div class="arrowright-wrapper"></div></div>
-<div class="smoothtool-process-item"><h3>Plan</h3>
-<p>Plan the whole process 
-from welding to finish 
-product</p>
-<div class="arrowright-wrapper"></div></div>
-<div class="smoothtool-process-item"><h3>Plan</h3>
-<p>Plan the whole process 
-from welding to finish 
-product</p>
-<div class="arrowright-wrapper"></div></div>
-</article>
+    <article class="smoothtool-maincontent-banner">
+        <div class="maincontent-banner-header"><h2>How SmoothTool Works</h2></div>
+    </article>
+    <article class="smoothtool-process-wrapper">
+    <div class="smoothtool-process-item">
+    <h3>Plan</h3>
+    <p>Plan the whole process 
+    from welding to finish 
+    product</p>
+    <div class="arrowright-wrapper"><i class="fas fa-long-arrow-alt-right"></i></div>
+    </div>
+    <div class="smoothtool-process-item"><h3>Plan</h3>
+    <p>Plan the whole process 
+    from welding to finish 
+    product</p>
+    <div class="arrowright-wrapper"><i class="fas fa-long-arrow-alt-right"></i></div></div>
+    <div class="smoothtool-process-item"><h3>Plan</h3>
+    <p>Plan the whole process 
+    from welding to finish 
+    product</p>
+    <div class="arrowright-wrapper"><i class="fas fa-long-arrow-alt-right"></i></div></div>
+    <div class="smoothtool-process-item"><h3>Plan</h3>
+    <p>Plan the whole process 
+    from welding to finish 
+    product</p>
+    <div class="arrowright-wrapper"><i class="fas fa-long-arrow-alt-right"></i></div></div>
+    <div class="smoothtool-process-item"><h3>Plan</h3>
+    <p>Plan the whole process 
+    from welding to finish 
+    product</p>
+    <div class="arrowright-wrapper"><i class="fas fa-long-arrow-alt-right"></i></div></div>
+    </article>
 </section>
 <section class="frontpage-info-aboutus">
 <article class="info-aboutus-row1">
