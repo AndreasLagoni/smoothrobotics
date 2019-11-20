df085ba3762af85207087a868f05c5e5bd2e4777
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-11-20 17:23:36 +0100

    lavede en masse custom fields

--- template-aboutus.php
+++ template-aboutus.php
@@ -8,27 +8,18 @@
 <article class="banneroverlay"></article>
 <article class="banner-innerwrapper">
 <h2 class="aboutus-h2">THE SMOOTHTOOL</h2>
-<h1 class="aboutus-h1"><?php echo get_theme_mod('smoothrobotics-aboutus-callout-headline', 'ACCELERATE YOUR COMPANYS PRODUCTION')?></h1>
+<h1 class="aboutus-h1"><?php echo get_post_meta($post->ID, 'header_title', true);?></h1>
 </article>
 </section>
 <main class="aboutuswrapper">
     <section class="aboutus-intro-wrapper">
         <article class="intro-content">
-            <h2><?php echo get_theme_mod('smoothrobotics-aboutus-callout-mainheadline', 'What we do
-        for you')?></h2>
-<p><?php echo get_theme_mod('smoothrobotics-aboutus-callout-maincontent','Smooth Robotics is born from the need to save time, money, and regrets in welding automatisation. <br><br>
-        Because nobody really needs a robot. You need a solution. Our goal is to perfect how to make robots work on the principle 
-        of human expertise in both quality and efficiency. 
-        <br><br>We want to make you able to use your experiences 
-        in the way your robot works with you. 
-        Detail and focus should never be lost in translation between 
-        human and machine. It should flow naturally. Smooth. 
-        We are a spin-out company from University of Southern 
-        Denmark and part of the Odense Robotics cluster. 
-        <br><br>Contact us! We would be happy to give you a try!' )?></p>
+            <h2><?php echo get_post_meta($post->ID, 'first_section_headline', true);?></h2>
+<p><?php echo get_post_meta($post->ID, 'first_section_content', true);?></p>
         </article>
-        <article class="intro-image">
-            <img src="http://localhost/wordpress/wp-content/uploads/2019/11/Group-248.png" alt="">
+        <article class="intro-image" style="background-image: url(<?php 
+            the_field('first_section_image');
+            ?>)">
         </article>
     </section>
     <section class="quoteimagewrapper">
@@ -37,39 +28,23 @@
             All need a solution.”</h2>
         </article>
         <article class="quoteimage">
-<img src="http://localhost/wordpress/wp-content/uploads/2019/11/Group-249.png" alt="">
+<img src="<?php 
+            the_field('second_section_image');
+            ?>" alt="">
         </article>
     </section>
     <section class="ourstorywrapper">
         <article class="ourstory-maininfo">
-        <h2>Our story</h2>
-        <p class="ourstory-info-p">We were founded in 2018, with a 
-    tiny group of people, with a very 
-    big ambition </p>
-        <div class="ourfocus">
-        <h3 class="ourstory-headh3">Our Focus?</h3>
-        <p class="ourstory-shortinfo">To automate the welding process</p>
-        </div>
-        <div class="ourgoal">
-    <h3 class="ourstory-headh3">Our Goal?</h3>
-    <p class="ourstory-shortinfo">To automate the welding process worldwide, and take part in 
-        making big productions accelerate and more productive.</p>
-        </div>
+        <h2><?php echo get_post_meta($post->ID, 'third_section_headline', true);?></h2>
+        <p class="ourstory-info-p">
+
+        <?php echo get_post_meta($post->ID, 'third_section_text_column1', true);?>
+
+        </p>
+        
         </article>
         <article class="ourstory-subinfo">
-            <p>Smooth Robotics is born from the need to save time, money, and regrets in welding automatisation. Because nobody really needs a robot. You need a solution. <br><br>
-
-Our goal is to perfect how to make robots work on the principle of human expertise in both quality and efficiency. We want to make you able to use your experiences in the way your robot works with you. Detail and focus should never be lost in translation between human and machine. It should flow naturally. Smooth.<br><br>
-
-We are a spin-out company from University of Southern Denmark and part of the Odense Robotics cluster.<br><br>
-
-Contact us! We would be happy to give you a try!<br><br>
-
-Smooth Robotics is born from the need to save time, money, and regrets in welding automatisation. Because nobody really needs a robot. You need a solution.<br><br>
-
-Our goal is to perfect how to make robots work on the principle of human expertise in both quality and efficiency. We want to make you able to use your experiences in the way your robot works with you. Detail and focus should never be lost in translation between human and machine. It should flow naturally. Smooth.<br><br>
-
-</p>
+            <p><?php echo get_post_meta($post->ID, 'third_section_text_column2', true);?></p>
         </article>
     </section>
     <section class="teammembers-wrapper">
