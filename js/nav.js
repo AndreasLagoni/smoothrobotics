691bce2f2a9b49bef4005eca1b564a134e7a68c5
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-12-02 20:36:55 +0100

    mere js nav

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,13 @@
 window.onscroll = function() {
   let mobileMenu = window.matchMedia("(max-width: 1200px)");
   if (mobileMenu.matches) {
+    scrollFunctionMobile();
   } else {
-    scrollFunction();
+    scrollFunctionWeb();
   }
 };
 
-function scrollFunction() {
+function scrollFunctionWeb() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.querySelector(".topNav-logo").style.height = "80px";
     document.getElementById("topheader").style.height = "80px";
@@ -22,5 +23,16 @@ function scrollFunction() {
     for (i = 0; i < navLinks.length; i++) {
       navLinks[i].style.fontSize = "25px";
     }
+  }
+}
+function scrollFunctionMobile() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector(".topNav-logo").style.height = "80px";
+    document.getElementById("topheader").style.height = "80px";
+    document.querySelector(".topNav-links").style.top = "80px";
+  } else {
+    document.querySelector(".topNav-logo").style.height = "110px";
+    document.getElementById("topheader").style.height = "110px";
+    document.querySelector(".topNav-links").style.top = "110px";
   }
 }
